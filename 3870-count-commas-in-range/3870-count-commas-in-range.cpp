e8db3abaea303b754037816494621771d89f2df8
abhaymaddheshiya80-gif <abhaymaddheshiya80@gmail.com>
2026-09-08 10:12:33 +0530

    Time: 29 ms (5.52%), Space: 8.6 MB (15.51%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countDigit(int n)
+    {
+        int count=0;
+        while(n)
+        {
+            n=n/10;
+            count++;
+        }
+        return count;
+    }
+    int countCommas(int n) {
+        int num=n;
+        int s=countDigit(n);
+       int  commas=s/3;
+        int res=0;
+        while(countDigit(n)>=4)
+        {
+         res=res+commas;
+         n--; 
+        }
+        if(num==100000)
+        {
+            return res/2;
+        }
+        return res;
+    }
+};
