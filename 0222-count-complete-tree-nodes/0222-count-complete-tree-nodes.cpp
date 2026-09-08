0d63e33e6fcbaa548a46270d4ec502f22a83d869
abhaymaddheshiya80-gif <abhaymaddheshiya80@gmail.com>
2026-09-08 09:51:22 +0530

    Time: 0 ms (100%), Space: 31.2 MB (74.19%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root==nullptr) return 0;
+        int leftHeight=0,rightHeight=0;
+        TreeNode*curr=root;
+        while(curr!=nullptr){
+            rightHeight++;
+            curr=curr->right;
+        }
+        curr=root;
+        while(curr!=nullptr)
+        {
+            leftHeight++;
+            curr=curr->left;
+        }
+        if(leftHeight==rightHeight)
+        {
+            return(1<<leftHeight)-1;
+        }
+        return 1+countNodes(root->left)+countNodes(root->right);
+    }
+};
